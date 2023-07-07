ab5f4d7533de5f49cb6c5d655feda54c8f9b286e
Philip Lu <philiplu97@gmail.com>
2023-07-06 20:09:30 -0400

    2-coin testing, with/without fee output

--- Cryptoswap-Price-Test.py
+++ Cryptoswap-Price-Test.py
@@ -155,7 +155,7 @@ def _newton_y(i: int, ANN: float, gamma: float, D: float, xp_unsorted: List[floa
 
     raise Exception(f"y did not converge: {mesg}")
 
-def get_dy(i: int, j: int, ANN: float, gamma: float, D: float, xp: List[float], dx: float, fee_params: List[float]) -> float:
+def get_dy(i: int, j: int, ANN: float, gamma: float, D: float, xp: List[float], dx: float, fee_params: List[float]) -> (float, float):
     """
     Calculate the amount received from swapping `dx`
     amount of the `i`-th coin for the `j`-th coin.
@@ -183,6 +183,8 @@ def get_dy(i: int, j: int, ANN: float, gamma: float, D: float, xp: List[float], 
     Returns
     -------
     float
+        The output of coin j
+    float 
         The output of coin j minus the swap fee
     
     Note
@@ -202,11 +204,11 @@ def get_dy(i: int, j: int, ANN: float, gamma: float, D: float, xp: List[float], 
     dy: float = xp_copy[j] - y
     xp_copy[j] = y
     
-    dy -= _fee(fee_params, xp_copy) * dy
+    dy_fee = dy - _fee(fee_params, xp_copy) * dy
     
-    return dy
+    return dy, dy_fee
 
-def get_dydx(i: int, j: int, ANN: float, gamma: float, D: float, xp: List[float], dx: float, fee_params: List[float]) -> float:
+def get_dydx(i: int, j: int, ANN: float, gamma: float, D: float, xp: List[float], dx: float, fee_params: List[float]) -> (float, float):
     """
     Calculate the effective swap price after trading 
     'dx' amount of the 'i'-th coin for dy of the 'j'-th coin.
@@ -234,18 +236,21 @@ def get_dydx(i: int, j: int, ANN: float, gamma: float, D: float, xp: List[float]
     -------
     float
         The output of coin j divided by the input of coin i
+    float
+        The output of coin j divided by the input of coin i minus the swap fee
 
     Note
     ----
     This is a "view" function; it doesn't change the state of the pool.
     """
-    dy: float = get_dy(i, j, ANN, gamma, D, xp, dx, fee_params)
+    dy, dy_fee = get_dy(i, j, ANN, gamma, D, xp, dx, fee_params)
     
     p: float = dy / dx
+    p_fee: float = dy_fee / dx
         
-    return p
+    return p, p_fee
 
-def spot_price(i: int, j: int, ANN: float, gamma: float, D: float, xp: List[float], fee_params: List[float]) -> float:
+def spot_price(i: int, j: int, ANN: float, gamma: float, D: float, xp: List[float], fee_params: List[float]) -> (float, float):
     """
     Calculate a spot price in coin j per coin i in D units.
     Simply differentiate the Cryptoswap equation F
@@ -274,6 +279,8 @@ def spot_price(i: int, j: int, ANN: float, gamma: float, D: float, xp: List[floa
     -------
     float
         The spot price of coin j per coin i given current xp
+    float
+        The spot price of coin j per coin i given current xp minus the swap fee
     """
     N = len(xp)
     
@@ -293,23 +300,16 @@ def spot_price(i: int, j: int, ANN: float, gamma: float, D: float, xp: List[floa
     _g1pk0 = gamma + 1 + K0
     _g1mk0 = gamma + 1 - K0 
     
-    above = xp_copy[i] * ((ANNG2 / (D * _g1mk0**2)) * ((_g1pk0 * (S - D)) / _g1mk0 + xp_copy[j]) + 1)
+    above: float = xp_copy[i] * ((ANNG2 / (D * _g1mk0**2)) * ((_g1pk0 * (S - D)) / _g1mk0 + xp_copy[j]) + 1)
     
-    below = xp_copy[j] * ((ANNG2 / (D * _g1mk0**2)) * ((_g1pk0 * (S - D)) / _g1mk0 + xp_copy[i]) + 1)
+    below: float = xp_copy[j] * ((ANNG2 / (D * _g1mk0**2)) * ((_g1pk0 * (S - D)) / _g1mk0 + xp_copy[i]) + 1)
     
     # Spot price in token j per token i
-    price_ji = above / below
+    price_ji: float = above / below
     
+    return price_ji, price_ji * (1 - _fee(fee_params, xp_copy))
     
-    # linear approximation of how much token j would be returned for an input of dx
-    # dy = price_ji * dx
-    # xp_copy[j] -= dy
-    
-    # dy -= _fee(fee_params, xp_copy) * dy
-    
-    return price_ji * (1 - _fee(fee_params, xp_copy))
-    
-def get_p(i: int, j: int, ANN: float, gamma: float, D: float, xp: List[float], fee_params: List[float]) -> float:
+def get_p(i: int, j: int, ANN: float, gamma: float, D: float, xp: List[float], fee_params: List[float]) -> (float, float):
     """
     "Optimized" spot price calculation in coin j per coin i in D units. 
     Implemented in onchain Tricrypto-NG pools and requires a 
@@ -339,6 +339,8 @@ def get_p(i: int, j: int, ANN: float, gamma: float, D: float, xp: List[float], f
     -------
     float
         The spot price of coin j per coin i given current xp
+    float
+        The spot price of coin j per coin i given current xp minus the swap fee
     """
     N = len(xp)
     
@@ -357,41 +359,62 @@ def get_p(i: int, j: int, ANN: float, gamma: float, D: float, xp: List[float], f
     # gamma**2 <= GK0 <= (gamma + 1)**2
     GK0 = 2 * K0**3 - (2 * gamma + 3) * K0**2 + (gamma + 1)**2
     
-    above = xp_copy[i] * (GK0 + ANNG2 * K0 * xp_copy[j] / D)
+    above: float = xp_copy[i] * (GK0 + ANNG2 * K0 * xp_copy[j] / D)
     
-    below = xp_copy[j] * (GK0 + ANNG2 * K0 * xp_copy[i] / D)
+    below: float = xp_copy[j] * (GK0 + ANNG2 * K0 * xp_copy[i] / D)
     
     # Spot price in token j per token i
-    price_ji = above / below
+    price_ji: float = above / below
     
-    # linear approximation of how much token j would be returned for an input of dx
-    # dy = price_ji * dx
-    # xp_copy[j] -= dy
-    
-    # dy -= _fee(fee_params, xp_copy) * dy
-    
-    return price_ji * (1 - _fee(fee_params, xp_copy))
+    return price_ji, price_ji * (1 - _fee(fee_params, xp_copy))
 
-# Tricrypto-2 (USDT-BTC-ETH)
+# Parameters taken from Tricrypto-2 (USDT-BTC-ETH) are indicated below
 
-N: int = 3
-
-ij: List[tuple] = list(permutations(range(N), r=2)) # all possible ordered i-j pairs as tuples
+Ns: List[int] = [2, 3]
 
 A_base: float = 6.3246 # taken from pool named above
 A_iter: int = 8 # num. of A to iterate through, including A_base  
 A_step: float = 2.5 # increment A by this much
-ANNs: List[float] = [A * N**N for A in [A_base + i * A_step for i in range(A_iter)]] 
 
 gamma_base: float = 1.1809167828997 # taken from pool named above (at 10**-5 decimals place) and adjusted to 10**1 decimals place
 
 # First list is within gamma's range of values: 10**-8 <= gamma <= 10**-2. Second list is to approximate Stableswap, which Cryptoswap becomes at gamma ~10
 gammas: List[float] = [gamma_base * 10**i for i in range(-8, -3 + 1)] + [gamma_base * 10**i for i in range(-2, 1 + 1)]
 
-# xp is normalized to token 0 units (not factoring in precision)
-# order for tricrypto-2: USDT, BTC, ETH 
+# taken from pool named above
+fee_gamma: float = 0.0005
+mid_fee: float = 0.0003 # 3 bps
+out_fee: float =  0.003 # 30 bps
+fee_params_lists: List[List[float]] = [[fee_gamma, mid_fee, out_fee],]
 
-# scenarios separated by line, each line ordered from least to most extreme imbalance: 
+# xp is normalized to token 0 units (not factoring in precision)
+# Slippage is assumed to benefit the pool
+
+# Each List[List[List[float]]] below groups lists of lists of balances for each of the pools' coins by the general degree of imbalance
+
+# In every list of list of balances, scenarios are separated by line, with each line ordered from 
+# least to most imbalanced: 
+
+# 2-coin crveth is an example (ETH-CRV):
+# perfect balance
+# eth dump
+# crv dump
+
+two_coin_xps: List[List[List[float]]] = [[[50_000_000, 50_000_000], \
+    [55_000_000, 45_500_000], [64_000_000, 37_300_000], \
+    [44_500_000, 56_000_000], [40_000_000, 61_000_000], \
+    ], \
+                                        [[50_000_000, 50_000_000], \
+    [100_000_000, 1_500_000], [130_000_000, 1_300_000], \
+    [1_900_000, 99_000_000], [124_000_000, 1_240_900], \
+    ], \
+                                        [[1, 1], \
+    [1.9, 0.11], [2.1, 0.021], \
+    [2, 0.08], [2.5, 0.025], \
+    ], \
+                                        ]
+
+# 3-coin tricrypto-2 is an example (USDT-BTC-ETH):
 # perfect balance
 # usdt dump: btc and eth equal
 # usdt dump: more btc bought than eth
@@ -400,82 +423,106 @@ gammas: List[float] = [gamma_base * 10**i for i in range(-8, -3 + 1)] + [gamma_b
 # btc/eth dump: more btc sold than eth  
 # btc/eth dump: more eth sold than btc
 
-# Slippage is assumed to benefit the pool
-
-# Extreme values
-
-# NEED MORE EXTREME - AT LEAST 1 BALANCE AS SINGLE DIGIT % OF ONE OTHER
-# also need n = 2
-
-'''
-xps: List[List[float]] = [[50_000_000, 50_000_000, 50_000_000], \
-    [60_000_000, 45_000_000, 45_000_000], [75_000_000, 39_000_000, 39_000_000], \
-    [53_000_000, 48_000_000, 49_250_000], [63_000_000, 41_000_000, 48_000_000], [78_000_000, 30_000_000, 46_000_000], \
-    [67_000_000, 46_200_000, 39_400_000], [74_000_000, 48_000_000 , 30_000_000], \
-    [35_500_000, 57_500_000, 57_500_000], [22_000_000, 65_000_000, 65_000_000], \
-    [42_000_000, 56_000_000, 51_750_000], [33_000_000, 64_500_000, 51_800_000], \
-    [48_250_000, 51_500_000, 50_200_000], [39_000_000, 54_250_000, 56_000_000], [44_000_000, 51_100_000, 55_700_000], \
-    ]
-'''
-
-xps: List[List[float]] = [[50_000_000, 50_000_000, 50_000_000], \
+three_coin_xps: List[List[List[float]]] = [[[50_000_000, 50_000_000, 50_000_000], \
     [50_900_000, 49_600_000, 49_600_000], [51_600_000, 49_300_000, 49_300_000], \
     [51_000_000, 49_300_000, 49_775_000], [53_000_000, 48_000_000, 49_250_000], \
     [50_500_000, 49_850_000, 49_675_000], [52_300_000, 49_600_000, 48_200_000], \
     [48_825_000, 50_600_000, 50_600_000], [47_500_000, 51_220_000, 51_220_000], \
     [49_500_000, 50_300_000, 50_210_000], [48_100_000, 51_470_000, 50_400_000], \
     [49_340_000, 50_150_000, 50_500_000], [48_250_000, 50_200_000, 51_500_000], \
-    ]
+    ], \
+                                      [[50_000_000, 50_000_000, 50_000_000], \
+    [60_000_000, 45_000_000, 45_000_000], [75_000_000, 39_000_000, 39_000_000], \
+    [63_000_000, 41_000_000, 48_000_000], [78_000_000, 30_000_000, 46_000_000], \
+    [67_000_000, 46_200_000, 39_400_000], [74_000_000, 48_000_000, 30_000_000], \
+    [35_500_000, 57_500_000, 57_500_000], [22_000_000, 65_000_000, 65_000_000], \
+    [42_000_000, 56_000_000, 51_750_000], [33_000_000, 64_500_000, 51_800_000], \
+    [39_000_000, 54_250_000, 56_000_000], [44_000_000, 51_100_000, 55_700_000], \
+    ], \
+                                        [[50_000_000, 50_000_000, 50_000_000], \
+    [152_000_000, 2_000_000, 2_000_000], [151_000_000, 1_560_000, 1_560_000], \
+    [150_000_000, 3_000_000, 4_500_000], [155_000_000, 2_000_000, 3_000_000], \
+    [150_700_000, 2_000_000, 1_500_000], [155_500_000, 3_000_000, 1_800_000], \
+    [2_500_000, 77_100_000, 77_100_000], [2_000_000, 77_250_000, 77_250_000], \
+    [1_700_000, 91_500_000, 57_000_000], [1_600_000, 100_000_000, 55_000_000], \
+    [1_900_000, 55_000_000, 96_000_000], [1_650_000, 65_000_000, 96_000_000], \
+    ], \
+                                          [[1, 1, 1], \
+    [2.9, 0.035, 0.035], [3.05, 0.03, 0.03], \
+    [3, 0.3, 0.45], [3.1, 0.09, 0.2], \
+    [2.999, 0.35, 0.2], [3.087, 0.043, 0.08], \
+    [0.04, 1.45, 1.45], [0.07, 1.5, 1.5], \
+    [0.04, 1.7, 1.297], [0.032, 1.8, 1.32], \
+    [0.36, 1.2, 1.77], [0.036, 1.3, 1.7], \
+    ], \
+                                          ]
+
+# map N to a list of lists of lists of N-length balances
+xps: dict[int: List[List[List[float]]]] = {}
+xps[2] = two_coin_xps
+xps[3] = three_coin_xps
 
 dx_iter: int = 4 # num. of dx to iterate through  
 dx_step: float = 10**-6 # increment dx by this much in token 0 units (not factoring in precision)
 dxs: List[float] = [i * dx_step for i in range(1, dx_iter + 1)]
 
-# taken from pool named above
-fee_gamma: float = 0.0005
-mid_fee: float = 0.0003 # 3 bps
-out_fee: float =  0.003 # 30 bps
-fee_params_lists: List[List[float]] = [[fee_gamma, mid_fee, out_fee],]
-
 # for csv output
 table = []
 
-# all possible ordered combinations of the parameters defined above
-for param_set in product(ij, ANNs, gammas, xps, dxs, fee_params_lists):
-    # indices in param_set: 0 - (i, j); 1 - ANN; 2 - gamma; 3 - xp; 4 - dx; 5 - [fee_gamma, mid_fee, out_fee]
-    i, j = param_set[0]
-    ANN = param_set[1]
-    gamma = param_set[2]
-    xp = param_set[3]
-    dx = param_set[4]
-    fee_params = param_set[5]
-    
-    notij = list(set(range(N)) - set((i, j)))
-    
-    D = _newton_D(ANN, gamma, xp)
-    
-    dydx = get_dydx(i, j, ANN, gamma, D, xp, dx, fee_params) # discretized derivative
-    dy = dydx * dx
-    simple_derivation = spot_price(i, j, ANN, gamma, D, xp, fee_params) # our formula
-    optimized_derivation = get_p(i, j, ANN, gamma, D, xp, fee_params) # onchain formula
-    
-    simple_derivation_delta = dydx - simple_derivation
-    optimized_derivation_delta = dydx - optimized_derivation
-    delta_diff = simple_derivation_delta - optimized_derivation_delta
-    
-    # Each variable matches the corresponding column label below
-    table.append([N, (i, j), ANN / N**N, gamma, fee_params, xp[i], xp[j], [xp[k] for k in notij], \
-        D, dx, dy, dydx, simple_derivation, optimized_derivation, simple_derivation_delta, optimized_derivation_delta, \
-        delta_diff])
-    
+for N in Ns:
+
+    ij: List[tuple] = list(permutations(range(N), r=2)) # all possible ordered i-j pairs as tuples
+
+    ANNs: List[float] = [A * N**N for A in [A_base + i * A_step for i in range(A_iter)]] 
+
+    for xp in xps[N]:
+
+        # all possible ordered combinations of the parameters defined above
+        for param_set in product(ij, ANNs, gammas, xp, dxs, fee_params_lists):
+            # indices in param_set: 0 - (i, j); 1 - ANN; 2 - gamma; 3 - xp; 4 - dx; 5 - [fee_gamma, mid_fee, out_fee]
+            i, j = param_set[0]
+            ANN = param_set[1]
+            gamma = param_set[2]
+            xp = param_set[3]
+            dx = param_set[4]
+            fee_params = param_set[5]
+            
+            notij = list(set(range(N)) - set((i, j)))
+            
+            D = _newton_D(ANN, gamma, xp)
+            
+            dydx, dydx_fee = get_dydx(i, j, ANN, gamma, D, xp, dx, fee_params) # discretized derivative
+            dy = dydx * dx
+            dy_fee = dydx_fee * dx
+            
+            simple_derivation, simple_derivation_fee = spot_price(i, j, ANN, gamma, D, xp, fee_params) # our formula
+            optimized_derivation, optimized_derivation_fee = get_p(i, j, ANN, gamma, D, xp, fee_params) # onchain formula
+            
+            simple_derivation_delta = dydx - simple_derivation
+            optimized_derivation_delta = dydx - optimized_derivation
+            delta_diff = simple_derivation_delta - optimized_derivation_delta
+        
+            simple_derivation_delta_fee = dydx_fee - simple_derivation_fee
+            optimized_derivation_delta_fee = dydx_fee - optimized_derivation_fee
+            delta_diff_fee = simple_derivation_delta_fee - optimized_derivation_delta_fee
+            
+            # Each variable matches the corresponding column label below
+            table.append([N, (i, j), ANN / N**N, gamma, fee_params, xp[i], xp[j], [xp[k] for k in notij], \
+                D, dx, dy, dydx, simple_derivation, optimized_derivation, simple_derivation_delta, \
+                          optimized_derivation_delta, delta_diff, \
+                          dy_fee, dydx_fee, simple_derivation_fee, optimized_derivation_fee, \
+                          simple_derivation_delta_fee, optimized_derivation_delta_fee, delta_diff_fee])
+
+# think of a possible better way than simply overwriting every time
 filepath = 'Cryptoswap-Price-Test.csv' 
 column_labels = ["N", "(i, j)", "A", "gamma", "[fee_gamma, mid_fee, out_fee]", "xp[i]", "xp[j]", "[xp[not i or j]]", \
-                 "D", "dx", "dy", "dy / dx", "Our Formula", "Onchain Formula", "Our Formula Delta", "Onchain Formula Delta", "Difference in Deltas"]
+                 "D", "dx", "dy", "dy / dx", "Our Formula", "Onchain Formula", "Our Formula Delta", "Onchain Formula Delta", "Difference in Deltas", "dy with Fee", "dy / dx with Fee", "Our Formula with Fee", "Onchain Formula with Fee", "Our Formula with Fee Delta", "Onchain Formula with Fee Delta", "Difference in Deltas with Fees"]
 df = DataFrame(data=table, columns=column_labels)
 df.to_csv(filepath)
 
-df.hist(column="Our Formula Delta")
-df.hist(column="Onchain Formula Delta")
+df.hist(column="Our Formula with Fee Delta")
+df.hist(column="Onchain Formula with Fee Delta")
+df.hist(column="Difference in Deltas with Fees")
 plt.show()
 
 '''
